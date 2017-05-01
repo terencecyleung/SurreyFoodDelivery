ff61f797b839128062b86e0b9933e4ebd9c8f61a
billxue9141 <billxue9141@hotmail.com>
2017-05-01 14:20:42 -0700

    customer orders page

---
+++ app/src/main/java/t27/surreyfooddeliveryapp/CustomerOrderActivity.java
@@ -0,0 +1,13 @@
+package t27.surreyfooddeliveryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customerorder);
+    }
+}

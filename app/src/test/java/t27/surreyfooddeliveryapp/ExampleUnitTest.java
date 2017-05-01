ced412445c40d0246fc8632522e310139e26a0cc
Terence Leung <Terence Leung>
2017-05-01 11:16:57 -0700

    commit

---
+++ app/src/test/java/t27/surreyfooddeliveryapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package t27.surreyfooddeliveryapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

52f0317efad6b839aba4e092e66c2a2124534bbd
billxue9141 <billxue9141@hotmail.com>
2017-05-02 12:19:51 -0700

    welcome page color theme - done

--- app/src/main/java/t27/surreyfooddeliveryapp/WelcomeActivity.java
+++ app/src/main/java/t27/surreyfooddeliveryapp/WelcomeActivity.java
@@ -9,7 +9,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_welcome);
     }
 }

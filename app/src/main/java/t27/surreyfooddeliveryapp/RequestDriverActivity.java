a59fffb26f37b0a330507bb532d995091f67a74d
Kent Huang <kenttttt0@gmail.com>
2017-05-01 21:15:33 -0700

    add requestDriver page

---
+++ app/src/main/java/t27/surreyfooddeliveryapp/RequestDriverActivity.java
@@ -0,0 +1,13 @@
+package t27.surreyfooddeliveryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_driver);
+    }
+}

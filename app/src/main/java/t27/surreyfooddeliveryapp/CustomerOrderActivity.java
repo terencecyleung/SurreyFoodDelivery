f662a24403fb78c3dfbbc42d266dfe3b4650a2de
Kent Huang <kenttttt0@gmail.com>
2017-05-02 18:04:15 -0700

    activity flow up to current order pages

--- app/src/main/java/t27/surreyfooddeliveryapp/CustomerOrderActivity.java
+++ app/src/main/java/t27/surreyfooddeliveryapp/CustomerOrderActivity.java
@@ -1,7 +1,9 @@
 package t27.surreyfooddeliveryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CustomerOrderActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class CustomerOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customerorder);
+    }
+
+    public void cust_placeOrder_click(View view) {
+
+        //TODO change true to validation function
+        if(true) {
+            //passed validation
+            Intent from_cust_order_to_current_order = new Intent(this, CurrentOrderActivity.class);
+            startActivity(from_cust_order_to_current_order);
+        }
+
+        //fails to place order
     }
 }
--- app/src/main/java/t27/surreyfooddeliveryapp/RequestDriverActivity.java
+++ app/src/main/java/t27/surreyfooddeliveryapp/RequestDriverActivity.java
@@ -1,7 +1,9 @@
 package t27.surreyfooddeliveryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RequestDriverActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class RequestDriverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_driver);
+    }
+
+    public void restaurant_order_click(View view) {
+        //TODO change true to validation function
+        if(true) {
+            //passed validation
+            Intent from_rest_order_to_current_order = new Intent(this, CurrentOrderActivity.class);
+            startActivity(from_rest_order_to_current_order);
+        }
+
+        //fails to place order
     }
 }

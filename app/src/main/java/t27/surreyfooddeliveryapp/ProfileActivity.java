7cf6c98832d0cd8723fd7addc60d86a1a86640c1
Kent Huang <kenttttt0@gmail.com>
2017-05-09 00:55:56 -0700

    logout fun

--- app/src/main/java/t27/surreyfooddeliveryapp/ProfileActivity.java
+++ app/src/main/java/t27/surreyfooddeliveryapp/ProfileActivity.java
@@ -1,9 +1,13 @@
 package t27.surreyfooddeliveryapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.gson.Gson;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -20,7 +24,9 @@ public class ProfileActivity extends AppCompatActivity {
 
     public void logout_click(View view) {
 
-        //TODo login out function
+        //login out function
+        logout_fun();
+
 
         Intent from_profile_to_logout = new Intent(this,WelcomeActivity.class);
         from_profile_to_logout.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -31,5 +37,13 @@ public class ProfileActivity extends AppCompatActivity {
         Intent from_profile_to_currentorder = new Intent(this,CurrentOrderActivity.class);
         from_profile_to_currentorder.putExtra("caller_activity", "ProfileActivity");
         startActivity(from_profile_to_currentorder);
+    }
+
+    private void logout_fun() {
+        SharedPreferences userInfo_Prefs =  getApplicationContext().getSharedPreferences(getString(R.string.User_infor), Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = userInfo_Prefs.edit();
+        prefsEditor.putString("userUID",null);
+        prefsEditor.putString("userObject", null);
+        prefsEditor.apply();
     }
 }
